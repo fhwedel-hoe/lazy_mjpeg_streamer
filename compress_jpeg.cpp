f1c0e3eaf48991e6d701a912f6781b388243c958
Hermann Höhne <hoehermann@gmx.de>
2026-02-17 20:58:23 +0100

    No need to check for more recent libjpeg versions.
    The remaining code variant works on Arch and Ubuntu.

--- compress_jpeg.cpp
+++ compress_jpeg.cpp
@@ -17,19 +17,6 @@ binary_data compress(const binary_data & buffer, const int width, const int heig
             TJSAMP_420, JPEG_QUALITY, TJFLAG_FASTDCT
         );
     } else if (colorSpace == TJCS_YCbCr) {
-        // TODO: fix ifdef – we should check the turbojpeg version here (Arch probably uses something more recent than Ubuntu 24)
-        #ifdef ARCHLINUX
-        tj3Set(_jpegCompressor, TJPARAM_SUBSAMP, TJSAMP_420);
-        tj3Set(_jpegCompressor, TJPARAM_QUALITY, JPEG_QUALITY);
-        tj3Set(_jpegCompressor, TJPARAM_FASTDCT, 1);
-        if (-1 == tj3CompressFromYUV8
-            (_jpegCompressor,
-            buffer.data(), width, 1, height, 
-            &compressedImage, &jpegSize
-        )) {
-            std::cerr << tjGetErrorStr() << std::endl;
-        }
-        #else
         if (-1 == tjCompressFromYUV(
             _jpegCompressor,
             buffer.data(), width, 1, height, TJSAMP_420, 
@@ -38,7 +25,6 @@ binary_data compress(const binary_data & buffer, const int width, const int heig
         )) {
             std::cerr << tjGetErrorStr() << std::endl;
         }
-        #endif
     }
     tjDestroy(_jpegCompressor);
     binary_data image_compressed(compressedImage,compressedImage+jpegSize);
