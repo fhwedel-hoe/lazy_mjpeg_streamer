d4f53f027fc6ed5e04c401a7598708bc82cf511a
Hermann Höhne <hoehermann@gmx.de>
2026-02-19 01:34:44 +0100

    Wrap all raw pointers in smart pointers for reliable clean-up.

--- camera_ffmpeg.hpp
+++ camera_ffmpeg.hpp
@@ -14,12 +14,10 @@ class Camera_ffmpeg : public Camera {
         virtual std::expected<RawImage, Camera::GrabError> grab_frame();
     private:
         void destroy();
-        void initialize();
-        const char * source = nullptr;
-        AVFormatContext * format_ctx = nullptr;
-        AVCodecParameters * codecpar = nullptr;
+        std::unique_ptr<AVFormatContext, void (*)(AVFormatContext *)> format_ctx = {nullptr, [](AVFormatContext *) {}};
+        AVCodecParameters * codecpar = nullptr; // this will be used to provide easy acces to a struct managed by format_ctx, so no explicit deleter on this one
         unsigned int video_stream_index = -1;
-        AVCodecContext * codec_ctx = nullptr;
-        AVPacket * packet = nullptr;
-        AVFrame * frame = nullptr;
+        std::unique_ptr<AVCodecContext, void (*)(AVCodecContext *)> codec_ctx = {nullptr, [](AVCodecContext *) {}};
+        std::unique_ptr<AVPacket, void (*)(AVPacket *)> packet = {nullptr, [](AVPacket *) {}};
+        std::unique_ptr<AVFrame, void (*)(AVFrame *)> frame = {nullptr, [](AVFrame *) {}};
 };
